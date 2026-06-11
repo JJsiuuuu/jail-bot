3aaae8398b1f822e5d2c46883e1ff837a7ffd332
JJsiuuuu <132344543+JJsiuuuu@users.noreply.github.com>
2026-06-11 03:56:51 +0100

    Add HTTP server to index.js for bot status

--- index.js
+++ index.js
@@ -153,3 +153,5 @@ client.on('interactionCreate', async interaction => {
 });
 
 client.login(TOKEN);
+const http = require('http');
+http.createServer((req, res) => res.end('Bot is running!')).listen(process.env.PORT || 3000);
